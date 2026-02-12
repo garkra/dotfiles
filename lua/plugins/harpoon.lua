82a731f0d0e0120a0734e132d16e4baf1f5c5b72
garkra <garrett.l.kraus@gmail.com>
2026-02-11 22:17:42 -0700

    formatting and stuff

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,24 +12,44 @@ return {
 			for _, item in ipairs(harpoon_files.items) do
 				table.insert(file_paths, item.value)
 			end
-			require("telescope.pickers").new({}, {
-				prompt_title = "Harpoon",
-				finder = require("telescope.finders").new_table({ results = file_paths }),
-				previewer = conf.file_previewer({}),
-				sorter = conf.generic_sorter({}),
-			}):find()
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({ results = file_paths }),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
 		end
 
-		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-		vim.keymap.set("n", "<leader>d", function() harpoon:list():remove() end)
-		vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end)
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>d", function()
+			harpoon:list():remove()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			toggle_telescope(harpoon:list())
+		end)
 
-		vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<C-j>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-k>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-l>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-;>", function()
+			harpoon:list():select(4)
+		end)
 
-		vim.keymap.set("n", "<C-S-J>", function() harpoon:list():prev() end)
-		vim.keymap.set("n", "<C-S-K>", function() harpoon:list():next() end)
+		vim.keymap.set("n", "<C-S-J>", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<C-S-K>", function()
+			harpoon:list():next()
+		end)
 	end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,12 +26,23 @@ return {
 
 		vim.lsp.enable({ "lua_ls", "ts_ls", "gopls", "pyright", "rust_analyzer" })
 
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover info" })
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find references" })
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
+		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(ev)
+				local opts = { buffer = ev.buf, remap = false }
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+				vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, opts)
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+			end,
+		})
 	end,
 }
