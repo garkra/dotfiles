4466508dbf472469dc1a895d798842e93a606621
garkra <garrett.l.kraus@gmail.com>
2026-02-11 17:43:22 -0700

    Update keybindings for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,8 +6,9 @@ return {
 	},
 	keys = {
 		{ '<leader>pf', function() require('telescope.builtin').find_files() end, desc = 'Telescope find files' },
-		{ '<C-p>', function() require('telescope.builtin').git_files() end, desc = 'Telescope find files' },
-		-- { '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'Telescope live grep' },
+		{ '<C-p>', function() require('telescope.builtin').git_files() end, desc = 'Telescope find git files' },
+		{ '<leader>pg', function() require('telescope.builtin').live_grep() end, desc = 'Telescope live grep' },
+		-- { '<leader>ps', function() require('telescope.builtin').grep_string( {search = vim.fn.input("Grep > ") }); end, desc = 'Telescope find files' },
 		-- { '<leader>fb', function() require('telescope.builtin').buffers() end, desc = 'Telescope buffers' },
 		-- { '<leader>fh', function() require('telescope.builtin').help_tags() end, desc = 'Telescope help tags' },
 	},
