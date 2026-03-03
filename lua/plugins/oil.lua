60dbefa4abe67f51ea97f9d440b96db7e2b9adbc
garkra <garrett.l.kraus@gmail.com>
2026-03-02 18:19:35 -0700

    open oil by default

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,13 +1,18 @@
 return {
 	"stevearc/oil.nvim",
+	lazy = false,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
 		{ "<leader>pv", "<cmd>Oil<cr>", desc = "Open file explorer" },
 		{ "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
 	},
 	opts = {
+		default_file_explorer = true,
 		view_options = {
 			show_hidden = true,
+		},
+		preview_win = {
+			update_on_cursor_moved = true,
 		},
 	},
 }
