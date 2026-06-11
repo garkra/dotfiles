8a798429c93be08880b3f58b3f81b61d414da658
garkra <garrett.l.kraus@gmail.com>
2026-06-11 09:43:06 -0600

    Show date modified in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,6 +8,10 @@ return {
 	},
 	opts = {
 		default_file_explorer = true,
+		columns = {
+			"icon",
+			{ "mtime", format = "%Y-%m-%d %H:%M" },
+		},
 		keymaps = {
 			["<leader>yf"] = {
 				callback = function()
@@ -19,9 +23,29 @@ return {
 				end,
 				desc = "Copy current directory path",
 			},
+			-- Toggle between sorting by name and by modification time
+			["<leader>st"] = {
+				callback = function()
+					local oil = require("oil")
+					_G.oil_sort_by_mtime = not _G.oil_sort_by_mtime
+					if _G.oil_sort_by_mtime then
+						oil.set_sort({ { "type", "asc" }, { "mtime", "desc" } })
+						vim.notify("Oil: sorting by mtime (newest first)", vim.log.levels.INFO)
+					else
+						oil.set_sort({ { "type", "asc" }, { "name", "asc" } })
+						vim.notify("Oil: sorting by name", vim.log.levels.INFO)
+					end
+				end,
+				desc = "Toggle oil sort (name / mtime)",
+			},
 		},
 		view_options = {
 			show_hidden = true,
+			-- Default sort: directories first, then newest-modified files first
+			sort = {
+				{ "type", "asc" },
+				{ "mtime", "desc" },
+			},
 		},
 		preview_win = {
 			update_on_cursor_moved = true,
